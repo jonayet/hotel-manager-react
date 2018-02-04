f2a5ae286fa4850f900abd941f4410ac1e426f8e
Jonayet Hossain <jonayet.bu@gmail.com>
2018-02-04 10:31:45 +0100

    material-ui added

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Button from 'material-ui/Button';
 
 export const App = () => (
   <div>
     Hotel Manager Component
+    <Button raised color="primary">
+      Hello World
+    </Button>
   </div>
 );
 
