9e70798fcf094a858da87c8b0a2a5d5faef38856
Jonayet Hossain <jonayet.bu@gmail.com>
2018-02-05 02:49:30 +0100

    github pages added

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hotel Manager SPA using React
+
+## Demo
+https://jonayet.github.io/hotel-manager-react/
--- src/app/Router.jsx
+++ src/app/Router.jsx
@@ -3,10 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import { Dashboard } from './dashboard/Dashboard';
 
+function path(url) {
+  return `${process.env.PUBLIC_URL || ''}${url}`;
+}
+
 export const Router = () => (
   <main>
     <Switch>
-      <Route exact path="/" component={Dashboard} />
+      <Route exact path={path('/')} component={Dashboard} />
     </Switch>
   </main>
 );
