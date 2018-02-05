4075b2ce1692a4d710d0af7a02061188151002e1
Jonayet Hossain <jonayet.bu@gmail.com>
2018-02-05 08:32:55 +0100

    base href updated

--- src/app/Router.jsx
+++ src/app/Router.jsx
@@ -3,14 +3,10 @@ import { Switch, Route } from 'react-router-dom';
 
 import { Dashboard } from './dashboard/Dashboard';
 
-function path(url) {
-  return `${process.env.PUBLIC_URL || ''}${url}`;
-}
-
 export const Router = () => (
   <main>
     <Switch>
-      <Route exact path={path('/')} component={Dashboard} />
+      <Route exact path="" component={Dashboard} />
     </Switch>
   </main>
 );
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import './main.scss';
 import { renderApp } from './app';
 
+const baseElems = document.getElementsByTagName('base'); // eslint-disable-line
+if (baseElems.length > 0) {
+  baseElems[0].href = `${process.env.PUBLIC_URL || '/'}`;
+}
 renderApp();
